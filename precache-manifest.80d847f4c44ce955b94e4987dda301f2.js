6aa4dc94a0d53750ead16b4fa07340f0b85b86d1
vitkhom <vitalii.s.khomenko@gmail.com>
2020-07-27 15:24:03 +0300

    Updates

---
+++ precache-manifest.80d847f4c44ce955b94e4987dda301f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff2ab98a594f9a95ca44b5530fdbae0",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "13f475d678e04d848cea",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.31bfea16.chunk.css"
+  },
+  {
+    "revision": "d778283c5fbfabd45700",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.4aa59707.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.4aa59707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f475d678e04d848cea",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.23d7bb89.chunk.js"
+  },
+  {
+    "revision": "e3f60b8770ae85b5a663",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.6ec4936c.js"
+  }
+]);
